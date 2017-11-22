81c6118acf1d50e31cb6fc142472c25be2a01238
Juan Gutierrez <gutierr3z@gmail.com>
2017-11-21 20:48:34 -0500

    practice

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service'
 
 @Component({
   selector: 'app-main',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  users:string[];
+
+  constructor( public dataService:DataService ) { 
+
+    this.users = this.dataService.getUsers();
+  }
 
   ngOnInit() {
   }
